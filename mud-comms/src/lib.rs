5d4ba513c8d4d072fc747e7abb2b0c15ee4f0ace
Michael Edwards <medwards@walledcity.ca>
2023-06-04 18:48:11 +0200

    Extract libc socket server impl
    With the FFI layer introduced we can do more behind-the-scenes without
    touching CircleMUD itself.
    
    `lib.rs` is getting pretty long and we expect to have multiple comms
    backends so first step is to extract it to leave just the FFI layer.

--- mud-comms/src/lib.rs
+++ mud-comms/src/lib.rs
@@ -1,46 +1,16 @@
-use core::num;
+mod socket_libc;
+
 use std::cmp::min;
 use std::ffi::CStr;
 use std::ffi::CString;
-use std::io::ErrorKind;
 use std::io::Read;
 use std::io::Write;
-use std::mem;
-use std::mem::size_of;
-use std::net::IpAddr;
-use std::net::Ipv4Addr;
 use std::os::raw::c_char;
 use std::os::raw::c_uchar;
 
-use libc::accept;
-use libc::bind;
-use libc::c_int;
-use libc::c_void;
-use libc::close;
-use libc::fcntl;
-use libc::fd_set;
-use libc::in_addr;
-use libc::listen;
-use libc::read;
-use libc::select;
-use libc::send;
-use libc::sockaddr;
-use libc::sockaddr_in;
-use libc::socket;
-use libc::timeval;
-use libc::AF_INET;
-use libc::FD_ISSET;
-use libc::FD_SET;
-use libc::F_GETFL;
-use libc::F_SETFL;
-use libc::INADDR_ANY;
-use libc::O_NONBLOCK;
-use libc::PF_INET;
-use libc::SOCK_STREAM;
-
 #[no_mangle]
-pub extern "C" fn new_descriptor_manager(port: u16) -> *mut DescriptorManager {
-    match DescriptorManager::new(port) {
+pub extern "C" fn new_descriptor_manager(port: u16) -> *mut socket_libc::DescriptorManager {
+    match socket_libc::DescriptorManager::new(port) {
         Ok(manager) => Box::into_raw(Box::new(manager)),
         // TODO log, then return an error code?
         Err(_) => std::ptr::null_mut(),
@@ -48,7 +18,7 @@ pub extern "C" fn new_descriptor_manager(port: u16) -> *mut DescriptorManager {
 }
 
 #[no_mangle]
-pub extern "C" fn close_descriptor_manager(manager: *mut DescriptorManager) -> i32 {
+pub extern "C" fn close_descriptor_manager(manager: *mut socket_libc::DescriptorManager) -> i32 {
     if manager.is_null() {
         return -1;
     }
@@ -61,7 +31,7 @@ pub extern "C" fn close_descriptor_manager(manager: *mut DescriptorManager) -> i
 }
 
 #[no_mangle]
-pub extern "C" fn block_until_descriptor(mut manager: *mut DescriptorManager) -> i32 {
+pub extern "C" fn block_until_descriptor(mut manager: *mut socket_libc::DescriptorManager) -> i32 {
     if manager.is_null() {
         return -1;
     }
@@ -76,7 +46,9 @@ pub extern "C" fn block_until_descriptor(mut manager: *mut DescriptorManager) ->
 }
 
 #[no_mangle]
-pub extern "C" fn new_descriptor(manager: *mut DescriptorManager) -> *mut Descriptor {
+pub extern "C" fn new_descriptor(
+    manager: *mut socket_libc::DescriptorManager,
+) -> *mut socket_libc::Descriptor {
     if manager.is_null() {
         return std::ptr::null_mut();
     }
@@ -91,8 +63,8 @@ pub extern "C" fn new_descriptor(manager: *mut DescriptorManager) -> *mut Descri
 
 #[no_mangle]
 pub extern "C" fn close_descriptor(
-    mut _manager: *mut DescriptorManager,
-    descriptor: *mut Descriptor,
+    mut _manager: *mut socket_libc::DescriptorManager,
+    descriptor: *mut socket_libc::Descriptor,
 ) -> i32 {
     if descriptor.is_null() {
         return -1;
@@ -105,8 +77,8 @@ pub extern "C" fn close_descriptor(
 
 #[no_mangle]
 pub extern "C" fn get_descriptor_hostname(
-    mut manager: *mut DescriptorManager,
-    mut descriptor: *mut Descriptor,
+    mut manager: *mut socket_libc::DescriptorManager,
+    mut descriptor: *mut socket_libc::Descriptor,
     read_point: *mut c_uchar,
     space_left: usize,
 ) -> isize {
@@ -134,8 +106,8 @@ pub extern "C" fn get_descriptor_hostname(
 
 #[no_mangle]
 pub extern "C" fn read_from_descriptor(
-    mut manager: *mut DescriptorManager,
-    mut descriptor: *mut Descriptor,
+    mut manager: *mut socket_libc::DescriptorManager,
+    mut descriptor: *mut socket_libc::Descriptor,
     read_point: *mut c_uchar,
     space_left: usize,
 ) -> isize {
@@ -163,8 +135,8 @@ pub extern "C" fn read_from_descriptor(
 
 #[no_mangle]
 pub extern "C" fn write_to_descriptor(
-    manager: *mut DescriptorManager,
-    descriptor: *mut Descriptor,
+    manager: *mut socket_libc::DescriptorManager,
+    descriptor: *mut socket_libc::Descriptor,
     content: *const c_char,
 ) -> isize {
     if manager.is_null() || descriptor.is_null() || content.is_null() {
@@ -179,215 +151,5 @@ pub extern "C" fn write_to_descriptor(
                 -1
             }
         }
-    }
-}
-
-pub struct DescriptorManager {
-    socket: c_int,
-}
-
-// byte ordering helpers from sys::common::net
-pub fn htons(u: u16) -> u16 {
-    u.to_be()
-}
-
-pub fn htonl(u: u32) -> u32 {
-    u.to_be()
-}
-
-pub fn ntohs(u: u16) -> u16 {
-    u16::from_be(u)
-}
-
-pub fn ntohl(u: u32) -> u32 {
-    u32::from_be(u)
-}
-
-impl DescriptorManager {
-    fn new(listen_port: u16) -> Result<DescriptorManager, std::io::Error> {
-        unsafe {
-            let s = socket(PF_INET, SOCK_STREAM, 0);
-            if s < 0 {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::socket failed"));
-            }
-            // set_sendbuf
-            let addr_in = sockaddr_in {
-                sin_family: AF_INET as u16,
-                sin_port: htons(listen_port),
-                sin_addr: in_addr {
-                    s_addr: htonl(INADDR_ANY),
-                }, // TODO: specify the address?
-                sin_zero: [0; 8],
-            };
-            let x = (&addr_in) as *const sockaddr_in;
-            let y = x as *const sockaddr;
-            if bind(s, y, size_of::<sockaddr_in>() as u32) < 0 {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::bind failed"));
-            }
-            // set nonblocking
-            let mut flags = fcntl(s, F_GETFL, 0);
-            if flags < 0 {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::fcntl failed"));
-            }
-            flags |= O_NONBLOCK;
-            if fcntl(s, F_SETFL, flags) < 0 {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::fcntl failed"));
-            }
-
-            // listen on the socket
-            if listen(s, 5) < 0 {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::listen failed"));
-            }
-            Ok(DescriptorManager { socket: s })
-        }
-    }
-
-    fn block_until_descriptor(&self) -> Result<(), std::io::Error> {
-        unsafe {
-            let mut input_set: fd_set = mem::zeroed();
-            let mut output_set: fd_set = mem::zeroed();
-            let mut exc_set: fd_set = mem::zeroed();
-            FD_SET(self.socket, &mut input_set as *mut fd_set);
-            if select(
-                self.socket + 1,
-                &mut input_set as *mut fd_set,
-                &mut output_set as *mut fd_set,
-                &mut exc_set as *mut fd_set,
-                std::ptr::null_mut(),
-            ) < 0
-            {
-                return dbg!(Err(std::io::Error::last_os_error()));
-            }
-        }
-        Ok(())
-    }
-
-    fn new_descriptor(&self) -> Result<Box<Descriptor>, std::io::Error> {
-        unsafe {
-            // Maybe use FD_ZERO?
-            let mut input_set: fd_set = mem::zeroed();
-            let mut output_set: fd_set = mem::zeroed();
-            let mut exc_set: fd_set = mem::zeroed();
-            let mut timeout: timeval = mem::zeroed();
-            FD_SET(self.socket, &mut input_set as *mut fd_set);
-            if select(
-                self.socket + 1,
-                &mut input_set as *mut fd_set,
-                &mut output_set as *mut fd_set,
-                &mut exc_set as *mut fd_set,
-                &mut timeout as *mut timeval,
-            ) < 0
-            {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::select failed"));
-            }
-
-            // there is a descriptor
-            let mut peer: sockaddr_in = mem::zeroed();
-            let mut peer_len = size_of::<sockaddr_in>();
-            let file_descriptor = accept(
-                self.socket,
-                &mut peer as *mut sockaddr_in as *mut sockaddr,
-                &mut peer_len as *mut usize as *mut u32,
-            );
-            if file_descriptor < 0 {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::accept failed"));
-            }
-
-            let ip_addr = IpAddr::V4(Ipv4Addr::from(ntohl(peer.sin_addr.s_addr)));
-            let hostname = dns_lookup::lookup_addr(&ip_addr)
-                .or::<std::io::Error>(Ok(ip_addr.to_string()))
-                .expect("lookup with ip fallback to be infallible");
-
-            Ok(Box::new(Descriptor {
-                file_descriptor,
-                hostname,
-            }))
-        }
-    }
-}
-
-impl Drop for DescriptorManager {
-    fn drop(&mut self) {
-        unsafe {
-            if close(self.socket) < 0 {
-                todo!("handle socket closing failures");
-            }
-        }
-    }
-}
-
-pub struct Descriptor {
-    file_descriptor: c_int,
-    hostname: String,
-}
-
-impl Drop for Descriptor {
-    fn drop(&mut self) {
-        unsafe {
-            if close(self.file_descriptor) < 0 {
-                todo!("handle descriptor closing failures");
-            }
-        }
-    }
-}
-
-impl Read for Descriptor {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        unsafe {
-            // Check if there is anything to read
-            let mut input_set: fd_set = mem::zeroed();
-            let mut output_set: fd_set = mem::zeroed();
-            let mut exc_set: fd_set = mem::zeroed();
-            let mut timeout: timeval = mem::zeroed();
-            FD_SET(self.file_descriptor, &mut input_set as *mut fd_set);
-            if select(
-                self.file_descriptor + 1,
-                &mut input_set as *mut fd_set,
-                &mut output_set as *mut fd_set,
-                &mut exc_set as *mut fd_set,
-                &mut timeout as *mut timeval,
-            ) < 0
-            {
-                return Err(std::io::Error::new(ErrorKind::Other, "libc::select failed"));
-            }
-
-            if FD_ISSET(self.file_descriptor, &input_set as *const fd_set) {
-                let retval = read(
-                    self.file_descriptor,
-                    buf.as_mut_ptr() as *mut c_void,
-                    buf.len(),
-                );
-                if retval < 0 {
-                    Err(std::io::Error::last_os_error())
-                } else {
-                    Ok(retval as usize)
-                }
-            } else {
-                Ok(0)
-            }
-        }
-    }
-}
-
-impl Write for Descriptor {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        unsafe {
-            let retval = send(
-                self.file_descriptor,
-                buf.as_ptr() as *const c_void,
-                buf.len(),
-                0,
-            );
-            if retval < 0 {
-                Err(std::io::Error::last_os_error())
-            } else {
-                Ok(retval as usize)
-            }
-        }
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        // No meaningful on-demand flushing of unix network sockets (ie TCP_NODELAY is forever)
-        Ok(())
     }
 }
---
+++ mud-comms/src/socket_libc.rs
@@ -0,0 +1,242 @@
+use std::io::ErrorKind;
+use std::io::Read;
+use std::io::Write;
+use std::mem;
+use std::mem::size_of;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+
+use libc::accept;
+use libc::bind;
+use libc::c_int;
+use libc::c_void;
+use libc::close;
+use libc::fcntl;
+use libc::fd_set;
+use libc::in_addr;
+use libc::listen;
+use libc::read;
+use libc::select;
+use libc::send;
+use libc::sockaddr;
+use libc::sockaddr_in;
+use libc::socket;
+use libc::timeval;
+use libc::AF_INET;
+use libc::FD_ISSET;
+use libc::FD_SET;
+use libc::F_GETFL;
+use libc::F_SETFL;
+use libc::INADDR_ANY;
+use libc::O_NONBLOCK;
+use libc::PF_INET;
+use libc::SOCK_STREAM;
+
+pub struct DescriptorManager {
+    pub(crate) socket: c_int,
+}
+
+// byte ordering helpers from sys::common::net
+pub fn htons(u: u16) -> u16 {
+    u.to_be()
+}
+
+pub fn htonl(u: u32) -> u32 {
+    u.to_be()
+}
+
+pub fn ntohl(u: u32) -> u32 {
+    u32::from_be(u)
+}
+
+impl DescriptorManager {
+    pub(crate) fn new(listen_port: u16) -> Result<DescriptorManager, std::io::Error> {
+        unsafe {
+            let s = socket(PF_INET, SOCK_STREAM, 0);
+            if s < 0 {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::socket failed"));
+            }
+            // set_sendbuf
+            let addr_in = sockaddr_in {
+                sin_family: AF_INET as u16,
+                sin_port: htons(listen_port),
+                sin_addr: in_addr {
+                    s_addr: htonl(INADDR_ANY),
+                }, // TODO: specify the address?
+                sin_zero: [0; 8],
+            };
+            if bind(
+                s,
+                &addr_in as *const sockaddr_in as *const sockaddr,
+                size_of::<sockaddr_in>() as u32,
+            ) < 0
+            {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::bind failed"));
+            }
+            // set nonblocking
+            let mut flags = fcntl(s, F_GETFL, 0);
+            if flags < 0 {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::fcntl failed"));
+            }
+            flags |= O_NONBLOCK;
+            if fcntl(s, F_SETFL, flags) < 0 {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::fcntl failed"));
+            }
+
+            // listen on the socket
+            if listen(s, 5) < 0 {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::listen failed"));
+            }
+            Ok(DescriptorManager { socket: s })
+        }
+    }
+
+    pub(crate) fn block_until_descriptor(&self) -> Result<(), std::io::Error> {
+        unsafe {
+            let mut input_set: fd_set = mem::zeroed();
+            let mut output_set: fd_set = mem::zeroed();
+            let mut exc_set: fd_set = mem::zeroed();
+            FD_SET(self.socket, &mut input_set as *mut fd_set);
+            if select(
+                self.socket + 1,
+                &mut input_set as *mut fd_set,
+                &mut output_set as *mut fd_set,
+                &mut exc_set as *mut fd_set,
+                std::ptr::null_mut(),
+            ) < 0
+            {
+                return dbg!(Err(std::io::Error::last_os_error()));
+            }
+        }
+        Ok(())
+    }
+
+    pub(crate) fn new_descriptor(&self) -> Result<Box<Descriptor>, std::io::Error> {
+        unsafe {
+            // Maybe use FD_ZERO?
+            let mut input_set: fd_set = mem::zeroed();
+            let mut output_set: fd_set = mem::zeroed();
+            let mut exc_set: fd_set = mem::zeroed();
+            let mut timeout: timeval = mem::zeroed();
+            FD_SET(self.socket, &mut input_set as *mut fd_set);
+            if select(
+                self.socket + 1,
+                &mut input_set as *mut fd_set,
+                &mut output_set as *mut fd_set,
+                &mut exc_set as *mut fd_set,
+                &mut timeout as *mut timeval,
+            ) < 0
+            {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::select failed"));
+            }
+
+            // there is a descriptor
+            let mut peer: sockaddr_in = mem::zeroed();
+            let mut peer_len = size_of::<sockaddr_in>();
+            let file_descriptor = accept(
+                self.socket,
+                &mut peer as *mut sockaddr_in as *mut sockaddr,
+                &mut peer_len as *mut usize as *mut u32,
+            );
+            if file_descriptor < 0 {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::accept failed"));
+            }
+
+            let ip_addr = IpAddr::V4(Ipv4Addr::from(ntohl(peer.sin_addr.s_addr)));
+            let hostname = dns_lookup::lookup_addr(&ip_addr)
+                .or::<std::io::Error>(Ok(ip_addr.to_string()))
+                .expect("lookup with ip fallback to be infallible");
+
+            Ok(Box::new(Descriptor {
+                file_descriptor,
+                hostname,
+            }))
+        }
+    }
+}
+
+impl Drop for DescriptorManager {
+    fn drop(&mut self) {
+        unsafe {
+            if close(self.socket) < 0 {
+                todo!("handle socket closing failures");
+            }
+        }
+    }
+}
+
+pub struct Descriptor {
+    pub(crate) file_descriptor: c_int,
+    pub(crate) hostname: String,
+}
+
+impl Drop for Descriptor {
+    fn drop(&mut self) {
+        unsafe {
+            if close(self.file_descriptor) < 0 {
+                todo!("handle descriptor closing failures");
+            }
+        }
+    }
+}
+
+impl Read for Descriptor {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        unsafe {
+            // Check if there is anything to read
+            let mut input_set: fd_set = mem::zeroed();
+            let mut output_set: fd_set = mem::zeroed();
+            let mut exc_set: fd_set = mem::zeroed();
+            let mut timeout: timeval = mem::zeroed();
+            FD_SET(self.file_descriptor, &mut input_set as *mut fd_set);
+            if select(
+                self.file_descriptor + 1,
+                &mut input_set as *mut fd_set,
+                &mut output_set as *mut fd_set,
+                &mut exc_set as *mut fd_set,
+                &mut timeout as *mut timeval,
+            ) < 0
+            {
+                return Err(std::io::Error::new(ErrorKind::Other, "libc::select failed"));
+            }
+
+            if FD_ISSET(self.file_descriptor, &input_set as *const fd_set) {
+                let retval = read(
+                    self.file_descriptor,
+                    buf.as_mut_ptr() as *mut c_void,
+                    buf.len(),
+                );
+                if retval < 0 {
+                    Err(std::io::Error::last_os_error())
+                } else {
+                    Ok(retval as usize)
+                }
+            } else {
+                Ok(0)
+            }
+        }
+    }
+}
+
+impl Write for Descriptor {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        unsafe {
+            let retval = send(
+                self.file_descriptor,
+                buf.as_ptr() as *const c_void,
+                buf.len(),
+                0,
+            );
+            if retval < 0 {
+                Err(std::io::Error::last_os_error())
+            } else {
+                Ok(retval as usize)
+            }
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        // No meaningful on-demand flushing of unix network sockets (ie TCP_NODELAY is forever)
+        Ok(())
+    }
+}
